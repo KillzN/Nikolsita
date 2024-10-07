a650aaece036368af7c70bcfad36b9c2884210a6
KillzN <scorpionsheep203@gmail.com>
2024-10-07 16:10:35 -0300

    KILL - BOT

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,26 +1,20 @@
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_tagall
-
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
   if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
+  global.dfail('admin', m, conn)
+  throw false
   }
-  const pesan = args.join` `;
-  const oi = `*NikolBot Te menciona* ${pesan}`;
-  let teks = `*⺀এ𝘔𝘦𝘯𝘤𝘪𝘰𝘯 𝘎𝘦𝘯𝘦𝘳𝘢𝘭!এ⺀ *\n\n ${oi}\n\n➥ 🕷 𝙼𝙴𝙽𝚂𝙰𝙹𝙴: *_\n`;
-  for (const mem of participants) {
-    teks += `🫵🏼 • @${mem.id.split('@')[0]}\n`;
+  let pesan = args.join` `
+  let oi = `✉️ 𝙈𝙀𝙉𝙎𝘼𝙅𝙀: ${pesan}\n👤 𝙈𝙄𝙀𝙈𝘽𝙍𝙊𝙎: _*${participants.length}*_`;
+  let teks = `⺀ꨄ 𝐌𝐄𝐍𝐂𝐈𝐎𝐍 𝐆𝐄𝐍𝐄𝐑𝐀𝐋 ꨄ${oi}\n\n➥ 🕷 𝙼𝙴𝙽𝚂𝙰𝙹𝙴:*_\n`
+  for (let mem of participants) {
+  teks += `┊ᡣ𐭩 @${mem.id.split('@')[0]}\n`}
+  teks += `╰───── 𝐍𝐢𝐤𝐨𝐥 𝐁𝐨𝐭 ─────╯
+`
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
-  teks += `└ *𝐍𝐢𝐤𝐨𝐥 𝐁𝐨𝐭*`;
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
-};
-handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
-handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación|putos)$/i;
-handler.admin = true;
-handler.group = true;
-export default handler;
+  handler.help = ['tagall <mesaje>','invocar <mesaje>']
+  handler.tags = ['group']
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
+  handler.admin = true
+  handler.group = true
+  export default handler
